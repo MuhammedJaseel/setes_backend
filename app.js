7a709aca378256c664fd508b3ff0063361ddfa6d
unknown <jaseelmanamulli@gmail.com>
2022-02-27 19:21:36 +0530

    Firsct commit for aws

--- app.js
+++ app.js
@@ -130,7 +130,7 @@ app.post("/ctaker/matchevent", ctakerAuth, ctakerAddEvent);
 app.get("/conn", getAllConnectedSocket);
 
 app.get("*", (req, res) => res.send("Hello World!"));
-const server = app.listen(process.env.PORT || 3010, () =>
+const server = app.listen(process.env.PORT || 80, () =>
   console.log("Started")
 );
 const wss = new WebSocket.Server({ server });
