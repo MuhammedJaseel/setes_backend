25ed4a150283c87ee6793a59ec6980464123b361
unknown <jaseelmanamulli@gmail.com>
2022-03-09 15:06:13 +0530

    seconds Commit

--- module/auth.js
+++ module/auth.js
@@ -1,6 +1,21 @@
+const { ObjectId } = require("mongodb");
+const { getTables } = require("./database");
+
 exports.mobileAuth = function (req, res, next) {
   console.log(req.headers);
-  next();
+  var _id;
+  try {
+    _id = ObjectId(req.headers.user_id);
+  } catch (e) {
+    res.status(502).send({ msg: "Database Error" });
+    return;
+  }
+  getTables("users", { filter: { _id }, project: { key: 1 } })
+    .then((user) => {
+      if (user[0].key === req.headers.key) next();
+      else res.status(401).send({ msg: "fUnauthorized" });
+    })
+    .catch(() => res.status(502).send({ msg: "Database Error" }));
 };
 
 exports.adminAuth = function (req, res, next) {
