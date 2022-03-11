eb35597a1dbb6071dde93dd4142cc5ead14d9d19
unknown <jaseelmanamulli@gmail.com>
2022-03-11 14:38:50 +0530

    seconds Commit

--- method/ctaker_login.js
+++ method/ctaker_login.js
@@ -1,12 +1,18 @@
 const { getTable } = require("../module/database");
 
 exports.ctakerLogin = (req, res) => {
+  var key = Math.random().toString();
   getTable("ctakers", req.body)
     .then((ctaker) => {
       if (ctaker === null) res.status(401).send({ msg: "Wrong credential" });
       else {
         delete ctaker.password;
-        res.send(ctaker);
+        putTable("admins", { _id: data[0]._id }, { $set: { key } })
+          .then(() => {
+            ctaker.key = key;
+            res.send(ctaker);
+          })
+          .catch((e) => res.status(502).send({ msg: "Database Error" }));
       }
     })
     .catch(() => res.status(502).send({ msg: "Database Error" }));
