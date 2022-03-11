dbfc94b40af8afcd1474582a2680c8696c289cd1
unknown <jaseelmanamulli@gmail.com>
2022-03-11 14:16:53 +0530

    seconds Commit

--- method/admin_login.js
+++ method/admin_login.js
@@ -1,14 +1,21 @@
 const { getTables } = require("../module/database");
 
 exports.adminLogin = (req, res) => {
-  console.log(req.body);
+  var key = Math.random().toString();
   getTables("admins", {
     filter: { user_name: req.body.user_name, password: req.body.password },
     project: { password: 0 },
   })
     .then((data) => {
       if (data.length == 0) res.status(400).send({ msg: "Wrong Input" });
-      else res.send(data[0]);
+      else {
+        putTable("admins", { _id: data._id }, { $set: { key } })
+          .then((data_2) => {
+            data.key = key;
+            res.send(data);
+          })
+          .catch((e) => res.status(502).send({ msg: "Database Error" }));
+      }
     })
     .catch((err) => res.status(502).send({ msg: "Database Error" }));
 };
