9085fbf11c0d6fdbff7e42a600ea90e44385ee09
unknown <jaseelmanamulli@gmail.com>
2022-03-18 23:14:49 +0530

    seconds Commit

--- method/ctaker_event.js
+++ method/ctaker_event.js
@@ -11,32 +11,35 @@ exports.ctakerAddEvent = async (req, res) => {
     return;
   }
   delete body.match_id;
-  await getTable("bookings", { _id })
-    .then(async (booking) => {
-      if (booking !== null) {
-        var events = booking.events;
-        var item = booking[body.item];
-        events.push(body);
-        item[body.who.team] = item[body.who.team] + 1;
-        var updateBody = {};
-        updateBody[body.item] = item;
-        updateBody.events = events;
-        putTable("bookings", { _id }, { $set: updateBody })
-          .then(() => {
-            res.send({ msg: "Succesfully updated" });
-            try {
-              _id = ObjectId(body.who._id);
-              getTable("users", { _id }).then((user) => {
-                updateBody = {};
-                updateBody[body.item] = (user[body.item] ?? 0) + 1;
-                putTable("users", { _id }, { $set: updateBody });
-              });
-            } catch (error) {}
-          })
-          .catch((e) => {
-            res.status(502).send({ msg: "Error: Getting booking table" });
-          });
-      } else res.status(502).send({ msg: "Error: Not a valid id" });
-    })
-    .catch(() => res.status(502).send({ msg: "Error: Getting booking table" }));
+  var table = body.status == "Started" ? "matchs_live" : "matchs_fulltime";
+  res.send("done");
+  console.log(body);
+  // await getTable(table, { _id })
+  //   .then(async (booking) => {
+  //     if (booking !== null) {
+  //       var events = booking.events;
+  //       var item = booking[body.item];
+  //       events.push(body);
+  //       item[body.who.team] = item[body.who.team] + 1;
+  //       var updateBody = {};
+  //       updateBody[body.item] = item;
+  //       updateBody.events = events;
+  //       putTable(table, { _id }, { $set: updateBody })
+  //         .then(() => {
+  //           res.send({ msg: "Succesfully updated" });
+  //           try {
+  //             _id = ObjectId(body.who._id);
+  //             getTable("users", { _id }).then((user) => {
+  //               updateBody = {};
+  //               updateBody[body.item] = (user[body.item] ?? 0) + 1;
+  //               putTable("users", { _id }, { $set: updateBody });
+  //             });
+  //           } catch (error) {}
+  //         })
+  //         .catch((e) => {
+  //           res.status(502).send({ msg: "Error: Getting booking table" });
+  //         });
+  //     } else res.status(502).send({ msg: "Error: Not a valid id" });
+  //   })
+  //   .catch(() => res.status(502).send({ msg: "Error: Getting booking table" }));
 };
