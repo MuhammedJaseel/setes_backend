e33101371f1a6693d52e0a46629daff34219f5c8
unknown <jaseelmanamulli@gmail.com>
2022-03-19 15:42:55 +0530

    seconds Commit

--- method/mobile_public.js
+++ method/mobile_public.js
@@ -1,5 +1,5 @@
 var ObjectId = require("mongodb").ObjectId;
-const { getTable } = require("../module/database");
+const { getTable, getTables } = require("../module/database");
 
 exports.mobileGetPublicProfile = (req, res) => {
   var _id;
@@ -10,10 +10,29 @@ exports.mobileGetPublicProfile = (req, res) => {
     return;
   }
 
-  getTable("users", { _id })
+  getTables("users", {
+    filter: { _id },
+    project: {
+      img: 1,
+      phone: 1,
+      email: 1,
+      blood_group: 1,
+      bootsize: 1,
+      district: 1,
+      fav_position: 1,
+      home_truf: 1,
+      prime: 1,
+      sex: 1,
+      strong_foot: 1,
+      t_shirt_size: 1,
+      zone: 1,
+      goals: 1,
+      ycs: 1,
+    },
+  })
     .then((user) => {
-      if (user === null) res.status(400).send({ msg: "User Not Found" });
-      else res.send(user);
+      if (user.length > 0) res.send(user[0]);
+      else res.status(400).send({ msg: "User Not Found" });
     })
     .catch(() => res.status(502).send({ msg: "Database Error" }));
 };
