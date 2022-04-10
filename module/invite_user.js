f0f9e5315e3e8ae76f0afb41e94e3e1587a856d2
Muhammed Jaseel <jaseelmanamulli@gmail.com>
2022-04-10 12:57:57 +0530

    Commiting from my device

--- module/invite_user.js
+++ module/invite_user.js
@@ -1,5 +1,7 @@
-exports.inviteUser = (req, res) => {
+const net = require("os").networkInterfaces();
 
+exports.inviteUser = (req, res) => {
+  console.log(net);
   res.send(
     "https://play.google.com/store/apps/details?id=com.cuownbe.setes_mobile"
   );
