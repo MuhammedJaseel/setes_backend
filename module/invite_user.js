6d60080c2f3135758c1342887782ff27e48faa70
Muhammed Jaseel <jaseelmanamulli@gmail.com>
2022-04-10 21:32:19 +0530

    Commiting from my device

--- module/invite_user.js
+++ module/invite_user.js
@@ -3,7 +3,13 @@ const { postTable } = require("./database");
 
 exports.inviteUser = async (req, res) => {
   const reqIp = requestIp.getClientIp(req);
-  await postTable("inivites", { piblicIp: reqIp, inviter: req.body.id });
+  const body = {
+    piblicIp: reqIp,
+    inviter: req.body.id,
+    created_at: new Date(),
+  };
+  console.log(body);
+  await postTable("inivites", body);
   res.send(
     "https://play.google.com/store/apps/details?id=com.cuownbe.setes_mobile"
   );
