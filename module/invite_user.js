720c55fc4df594fbe03ea0b588b6f828001290d1
Muhammed Jaseel <jaseelmanamulli@gmail.com>
2022-04-10 13:07:06 +0530

    Commiting from my device

--- module/invite_user.js
+++ module/invite_user.js
@@ -1,5 +1,5 @@
 exports.inviteUser = (req, res) => {
-  console.log(req.socket.remoteAddress);
+  console.log(req.headers);
   console.log(req.connection.remoteAddress);  res.send(
     "https://play.google.com/store/apps/details?id=com.cuownbe.setes_mobile"
   );
